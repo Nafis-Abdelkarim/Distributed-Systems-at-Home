bcb91a66b818847cf134485a74ac5bae15059825
ANEM\a.nefis <A.NEFIS@anem.dz>
2025-08-23 20:15:13 +0100

    grpc service setup

---
+++ GrpcServiceA/Services/SumService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace GrpcServiceA.Services
+{
+    public class SumService : sum.sumBase     
+    {
+        private readonly ILogger<SumService> _logger;
+        public SumService(ILogger<SumService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AddResponse> SumTwoNumbers(AddRequest request, ServerCallContext serverCall)
+        {
+            return Task.FromResult(new AddResponse
+            {
+                Result = request.Number1 + request.Number2
+            });
+        }
+    }
+}
